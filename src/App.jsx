71be45e4ccdfe003360f117ec1fea1bc3406832d
naman <fullstackctp@gmail.com>
2023-09-03 20:15:14 +0530

    feat: components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold ">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
